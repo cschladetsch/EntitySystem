2426ea8d05eb05562c19fdd50e85172f1006e0f6
Christian <christian@twobulls.com>
2017-01-23 17:33:22 +1100

    Fixed 'optimisations' so it works correctly again.

---
+++ Assets/App/PidScalarControllerBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace App
+{
+	public class PidScalarControllerBehaviour : MonoBehaviour
+	{
+		public float SetPoint;
+		public float P = 0.5f;
+		public float I = 0.05f;
+		public float D = 0.1f;
+		private void Awake()
+		{
+			_controller = new App.Math.PidScalarController(P, I, D); 
+		}
+
+		private void Start()
+		{
+		}
+
+		private void Update()
+		{
+		}
+
+		private void FixedUpdate()
+		{
+			var offset = _controller.Calculate(SetPoint, transform.position.z, 0.1f);
+			if (_count < 100)
+				Debug.LogFormat("[{0}]: val:{1}, inc: {2}", _count++, transform.position.z, offset.ToString("F3"));
+
+			var p = transform.position;
+			var val = p.z + (float)offset;
+			transform.position = new Vector3(p.x, p.y, val);
+		}
+		int _count;
+
+		private App.Math.PidScalarController _controller;
+	}
+}
+
---
+++ Assets/App/PidVector3ControllerBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+using UnityEngine.Assertions;
+
+using App.Math;
+
+namespace App
+{
+	public class PidVector3ControllerBehaviour : MonoBehaviour
+	{
+		public Vector3 Target;
+
+		// TODO: may want different co-efficients for the different axes
+		public float P = 0.1f;
+		public float I = 0.5f;
+		public float D = 0.01f;
+
+		private void Awake()
+		{
+			_controller = new PidVector3Controller(P, I, D);
+			_controller.SetPoint = Target;
+		}
+
+		private void FixedUpdate()
+		{
+			var output = _controller.Calculate(transform.position, Time.fixedDeltaTime);
+			transform.position = output;
+		}
+
+		private PidVector3Controller _controller;
+	}
+}
+
