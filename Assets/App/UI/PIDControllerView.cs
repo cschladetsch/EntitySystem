8fb159fe99640d2794dc05070fd3252c1a5c631a
Christian <christian@twobulls.com>
2017-01-23 19:55:58 +1100

    Add view controller for PID systems.

---
+++ Assets/App/UI/PIDControllerView.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+
+using App.Math;
+
+using TMPro;
+
+namespace App
+{
+	public class PIDControllerView : MonoBehaviour
+	{
+		public TextMeshProUGUI ObjectName;
+		public PIDGainValueView Pgain;
+		public PIDGainValueView Igain;
+		public PIDGainValueView Dgain;
+
+		struct PidEntry
+		{
+		}
+		
+		void Start()
+		{
+		}
+
+		public void Configure(PidVector3ControllerBehaviour controller)
+		{
+			ObjectName.text = controller.name;
+		}
+
+		public void TargetPressed(Button sender)
+		{
+		}
+
+		public void ResetPressed(Button sender)
+		{
+		}
+
+		private PidScalarController _controller;
+	}
+}
+
--- Assets/App/UI/PIDGainValueView.cs
+++ Assets/App/UI/PIDGainValueView.cs
@@ -4,12 +4,19 @@ using System.Collections.Generic;
 using System.Linq;
 
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Assertions;
 
-namespace Sim.Physical
+using App.Math;
+
+namespace App
 {
-	public class TurningForce : MonoBehaviour
+	public class PIDGainValueView : MonoBehaviour
 	{
+		public string Name;
+		public Text Value;
+		public Slider Slider;
+
 		private void Awake()
 		{
 		}
